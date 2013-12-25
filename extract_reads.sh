4fea9c702511b16c76151017285ad026bf97d0bc
Likit Preeyanon <preeyano@msu.edu>
2013-12-24 20:08:38 -0500

    refactor code

--- extract_reads.sh
+++ extract_reads.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+function usage {
+    printf "Usage: %s <BAM file>\n" $0
+    printf "A BAM file is required.\n"
+} >&2
+
 if [ $# -lt 1 ]
 then
-    printf "Usage: extract_reads.sh <BAM file>\n"
-    printf "A BAM file is not required.\n"
+    usage
     exit 1
 fi
 
 if [ ! -e "$1".bai ]
 then
-    printf "BAM index file is not found, indexing it...\n"
+    printf "BAM index file is not found, indexing it...\n" >&2
     samtools index "$1"
 fi
 
@@ -17,12 +21,12 @@ outdir=$(dirname "$1")
 
 if [ $# -eq 2 ]
 then
-    printf "A list of chromosome is provided in %s.\n" "$2"
+    printf "A list of chromosome is provided in %s.\n" "$2" >&2
     for chr in $(cat "$2")
     do
-        printf "\textracting reads for %s..." "$chr"
+        printf "\textracting reads for %s..." "$chr" >&2
         samtools view -o "$outdir"/"$chr".sam "$1" "$chr"
-        printf "done.\n"
+        printf "done.\n" >&2
     done
 else
     samtools view -o "$outdir"/"reads".sam "$1"
