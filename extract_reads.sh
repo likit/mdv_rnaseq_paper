460cbfbd716e711d67dda751651d448ee4a2848e
Likit Preeyanon <preeyano@msu.edu>
2013-12-25 20:35:10 -0500

    Make the script to require two parameters.
    The script needs a bam file and a text file containing a list of chromosomes one line per chromosome.

--- extract_reads.sh
+++ extract_reads.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 function usage {
-    printf "Usage: %s <BAM file>\n" $0
+    printf "Usage: %s <BAM file> <chromosomes.txt>\n" $0
     printf "A BAM file is required.\n"
 } >&2
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
     usage
     exit 1
@@ -13,21 +13,15 @@ fi
 
 if [ ! -e "$1".bai ]
 then
-    printf "BAM index file is not found, indexing it...\n" >&2
+    printf "BAM index file is not found, creating it...\n" >&2
     samtools index "$1"
 fi
 
 outdir=$(dirname "$1")
 
-if [ $# -eq 2 ]
-then
-    printf "A list of chromosome is provided in %s.\n" "$2" >&2
-    for chr in $(cat "$2")
-    do
-        printf "\textracting reads for %s..." "$chr" >&2
-        samtools view -b -o "$outdir"/"$chr".bam "$1" "$chr"
-        printf "done.\n" >&2
-    done
-else
-    samtools view -b -o "$outdir"/"reads".bam "$1"
-fi
+for chr in $(cat "$2")
+do
+    printf "extracting reads for %s..." "$chr" >&2
+    samtools view -b -o "$outdir"/"$chr".bam "$1" "$chr"
+    printf "done.\n" >&2
+done
