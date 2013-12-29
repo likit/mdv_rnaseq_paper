67f26b9c971754c544fdb60992e3b63a10f8e5b1
Likit Preeyanon <preeyano@msu.edu>
2013-12-29 15:18:25 -0500

    update blat_job.sh for multiple inputs

--- blat_job.sh
+++ blat_job.sh
@@ -5,4 +5,4 @@
 #PBS -N Blat_${PBS_JOBID}
 
 cd ${PBS_O_WORKDIR}
-~/blat -noHead -extendThroughN -out=psl gal4selected.2bit all.fa.clean.nr all.fa.clean.nr.psl
+~/blat -noHead -extendThroughN -out=psl gal4selected.2bit ${input} ${input}.psl
