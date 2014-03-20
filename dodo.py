6f5ddea41fbba5982c26a3bd33c05d4087d35182
Likit Preeyanon <preeyano@msu.edu>
2014-03-19 22:44:17 -0400

    update dodo.py

--- dodo.py
+++ dodo.py
@@ -43,8 +43,8 @@ def task_prepare_for_blast2go_cuff_ref():
             }
 
 
-def task_blast_cuff_ref():
-    '''prepare sequences and alignments for blast2go'''
+def task_blast_cuff_ref_human():
+    '''run BLAST'''
 
     os.chdir(doit.initial_workdir)
     os.chdir('tophat/merged_cuff_ref')
@@ -54,9 +54,51 @@ def task_blast_cuff_ref():
                 ' ~/mdv-protocol/blast.sh' % (sample, sample)
 
         cmd2 = 'qsub -v db="Human_prot",input="%su_vs_i.cuffref.degenes.fdr.05.fa.longest"' \
-                ',program="blastx",output="%su_vs_i.cuffref.degenes.fdr.05.fa.longest.xml"' \
+                ',program="blastx",output="%su_vs_i.cuffref.degenes.fdr.05.fa.longest.gallus.xml"' \
                 ' ~/mdv-protocol/blast.sh' % (sample, sample)
 
         yield {'name':sample,
                 'actions':[cmd1, cmd2,]
+            }
+
+def task_blast_cuff_ref_gallus():
+    '''run BLAST'''
+
+    os.chdir(doit.initial_workdir)
+    os.chdir('tophat/merged_cuff_ref')
+    for sample in ['line6', 'line7']:
+        cmd1 = 'qsub -v db="Gallus_prot",input="%su_vs_i.cuffref.degenes.fdr.05.fa.prot.longest"' \
+                ',program="blastp",output="%su_vs_i.cuffref.degenes.fdr.05.fa.prot.longest.xml"' \
+                ' ~/mdv-protocol/blast.sh' % (sample, sample)
+
+        cmd2 = 'qsub -v db="Gallus_prot",input="%su_vs_i.cuffref.degenes.fdr.05.fa.longest"' \
+                ',program="blastx",output="%su_vs_i.cuffref.degenes.fdr.05.fa.longest.gallus.xml"' \
+                ' ~/mdv-protocol/blast.sh' % (sample, sample)
+
+        yield {'name':sample,
+                'actions':[cmd2,]
+            }
+
+def task_blast2go_cuff_ref():
+    '''run blast2go'''
+
+    os.chdir(doit.initial_workdir)
+    os.chdir('tophat/merged_cuff_ref')
+    for sample in ['line6', 'line7']:
+        cmd1 = 'qsub -v input="%su_vs_i.cuffref.degenes.fdr.05.fa.prot.longest.xml"' \
+                ',outdir="%su_cuffref_prot_blast2go_outdir"' \
+                ' ~/mdv-protocol/b2g_job.sh' % (sample, sample)
+        cmd2 = 'qsub -v input="%su_vs_i.cuffref.degenes.fdr.05.fa.longest.xml"' \
+                ',outdir="%su_cuffref_nucl_blast2go_outdir"' \
+                ' ~/mdv-protocol/b2g_job.sh' % (sample, sample)
+
+        yield {'name':sample,
+                'actions':[cmd1, cmd2,]
+            }
+
+def task_filter_low_isopct():
+    os.chdir(doit.initial_workdir)
+    cmd = 'python ~/mdv-protocol/filter-low-isopct.py 1.0 asm_cuff_ref_models.bed *cuffref*isoforms.results > asm_cuff_ref_models.flt.bed'
+    return {
+            'actions': [cmd],
             }
