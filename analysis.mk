c795d5982701a43af96d45865a3c4ddc095cfc01
Likit Preeyanon <preeyano@msu.edu>
2014-09-23 14:07:45 -0400

    added rules for KEGG and ontology analysis

--- analysis.mk
+++ analysis.mk
@@ -2,3 +2,13 @@ annotate:
 
 	Rscript $(protocol)/degenes_annotation.R
 	Rscript $(protocol)/degenes_annotation_human.R
+
+run-kegg:
+
+	# Rscript $(protocol)/goseq_line6.R
+	# Rscript $(protocol)/goseq_line7.R
+	Rscript $(protocol)/make_kegg_cleveland_plot.R
+
+run-bp-ontology:
+
+	Rscript $(protocol)/goseq_BP_up_down_both_lines.R
